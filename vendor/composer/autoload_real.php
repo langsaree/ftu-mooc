8a7a997daadbae589c86ca5ed3f31425ea44597a
Yamoo <52197011+Hiyam-1998@users.noreply.github.com>
2020-11-02 21:09:49 +0700

    Create new files of html
    create file dashboard.html for admin that use style AdminLTE

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit3fdba843a00090e8edc44815ffc9bdf0
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
