662b859547eecdfe3fa78c75a687c05bfca989dd
Yamoo <52197011+Hiyam-1998@users.noreply.github.com>
2021-01-29 22:28:27 +0700

    add file for CKEditor

---
+++ resources/views/editor.blade.php
@@ -0,0 +1,25 @@
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>CKEditor</title>
+</head>
+<body>
+<div class="container">
+    <br>
+<form action="">
+           <label>hello</label>
+    <input type="text" name="title" id="title">
+    <br>
+    <textarea name="editor"
+    <script>
+        // Replace the <textarea id="editor1"> with a CKEditor 4
+        // instance, using default configuration.
+        CKEDITOR.replace( 'editor1' );
+    </script>
+</form>
+</div>
+</body>
+</html>
